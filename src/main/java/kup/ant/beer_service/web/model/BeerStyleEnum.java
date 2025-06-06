6785afc3d005314069d1e2ca8bfc7b7b6e9c7221
Anton <kuprianov.anton@yandex.ru>
2025-06-07 00:18:55 +0400

    Add web data model #2

---
+++ src/main/java/kup/ant/beer_service/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package kup.ant.beer_service.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON;
+}
