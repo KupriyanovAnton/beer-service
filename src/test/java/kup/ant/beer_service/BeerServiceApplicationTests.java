a748f3692a923c758100f61f54374429d4431b4b
Anton <kuprianov.anton@yandex.ru>
2025-06-06 22:47:49 +0400

    Init commit Close #1

---
+++ src/test/java/kup/ant/beer_service/BeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kup.ant.beer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
